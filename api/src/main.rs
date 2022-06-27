0ee253123ee05f315c668abd90d8a32efeeed013
hidekazoo <tanakahidek@gmail.com>
2022-06-28 01:24:48 +0900

    fix tracing setting

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,12 +2,16 @@ use api::configuration::get_configuration;
 use api::startup::run;
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
-use tracing::info;
-use tracing_subscriber;
+use tracing::{info, Level};
+use tracing_subscriber::{self, FmtSubscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    tracing_subscriber::fmt::init();
+    // tracing_subscriber::fmt::init();
+    let subscriber = FmtSubscriber::builder()
+    .with_max_level(Level::TRACE)
+    .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
